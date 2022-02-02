19c1694c126493163dce3a47c2e0429071b81b92
Twol <tonywhalley@t-online.de>
2022-02-02 16:22:17 +0100

    servicehisilicon - convert to sigc3

--- servicehisilicon/servicehisilicon.h
+++ servicehisilicon/servicehisilicon.h
@@ -233,7 +233,7 @@ private:
 	errorInfo m_errorInfo;
 	std::string m_download_buffer_path;
 	eServiceHisilicon(eServiceReference ref);
-	sigc::signal2<void,iPlayableService*,int> m_event;
+	sigc::signal<(void,iPlayableService*,int)> m_event;
 	enum
 	{
 		stIdle, stRunning, stStopped,
